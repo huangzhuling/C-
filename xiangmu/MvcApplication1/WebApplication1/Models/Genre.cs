87e6ff5a4aeddc4080607df488be6a7810c6ad13
huangzhuling <643146111@qq.com>
2019-01-09 12:14:18 +0800

    32
    123

---
+++ xiangmu/MvcApplication1/WebApplication1/Models/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Genre
+    {
+        public string Name { get; set; }
+    }
+    public class Album
+    {
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
